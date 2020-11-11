743f1cfab6384333fb2170332ebbd87236cb58e1
plony benav <menachemfuterfas@gmail.com>
2020-11-11 18:25:31 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,12 +13,12 @@ def loop_message_n_times(message, n)
 end
 
 
-def output_array(number, array)
-  number.times do
+def output_array(array)
+
   counter = 0
 while counter < array.length do
   puts array[counter]
   counter += 1
-  end
+
 end
 end
